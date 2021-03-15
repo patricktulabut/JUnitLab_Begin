4baa6596ecdc26d663b21c8c47ee49cd0303422e
Patrick Tulabut <patricktulabut1@gmail.com>
2021-03-14 19:53:44 -0700

    git add GiftCardTest to files

---
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -0,0 +1,50 @@
+package csc131.junit;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class GiftCardTest {
+
+	@Test
+	public void testGetIssuingStore() 
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getIssuingStore()", issuingStore, card.getIssuingStore());
+
+	}
+	
+	@Test
+	public void testGetBalance()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		assertEquals("getBalance()", balance, card.getBalance(), 0.001);
+	}
+	
+	@Test 
+	public void deduct_RemainingBalance()  
+	{  
+		double balance;  
+		GiftCard card;  
+		int issuingStore;  
+		String s;  
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		s = "Remaining Balance: " + String.format("%6.2f",  90.00);
+		assertEquals("deduct(10.00)", s, card.deduct(10.00));
+	}
+	 
+}
